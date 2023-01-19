ab891f2c155e58eb07769c676a26102522eb9429
ram1point0 <ra.enomics@gmail.com>
2023-01-19 13:18:30 -0800

    initial commit

---
+++ helper-hardhat-config.js
@@ -0,0 +1,26 @@
+const networkConfig = {
+    31337: {
+        name: "localhost",
+    },
+    // Price Fee Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
+    5: {
+        name: "goreli",
+        ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
+    },
+    137: {
+        name: "polygon",
+        ethUsdPriceFeed: "0xF9680D99D6C9589e2a93a78A04A279e509205945",
+    },
+
+}
+
+const developmentChains = ["hardhat", "localhost"]
+const DECIMALS = 8
+const INITIAL_ANSWER = 200000000000
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+    DECIMALS,
+    INITIAL_ANSWER,
+}
